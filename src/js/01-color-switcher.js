e6ece0c2289370a72bfd0b17ecd71e6b18245a75
Pawel Gorniak <p_gorniak@tlen.pl>
2023-01-27 18:39:07 +0100

    Update JS

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let backgroundColorChange = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  backgroundColorChange = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(backgroundColorChange);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
